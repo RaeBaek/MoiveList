2f56dfabdc514465ad5a59a53653b5b763064b19
RaeBaek <bmp117@naver.com>
2023-08-09 16:54:18 +0900

    SeSAC 4주 2일차 (230808 .Tue) 과제 완료
    Kakao 도서 API를 활용한 책 도서 검색
    1. 도서 기능 검색
    2. 페이지네이션 기능 (UITableViewDataSourcePrefetching Protocol 활용)

---
+++ MoiveList/Controller/LibraryTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  LibraryTableViewCell.swift
+//  MoiveList
+//
+//  Created by 백래훈 on 2023/08/09.
+//
+
+import UIKit
+
+class LibraryTableViewCell: UITableViewCell {
+
+    static let identifier = "LibraryTableViewCell"
+    
+    @IBOutlet var posterImageView: UIImageView!
+    @IBOutlet var bookTitleLabel: UILabel!
+    @IBOutlet var publisherLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var saleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        posterImageView.contentMode = .scaleToFill
+        bookTitleLabel.font = .systemFont(ofSize: 17, weight: .bold)
+        publisherLabel.font = .systemFont(ofSize: 15, weight: .regular)
+        dateLabel.font = .systemFont(ofSize: 15, weight: .regular)
+        priceLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        saleLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+
+}
--- MoiveList/Controller/SearchViewController.swift
+++ MoiveList/Controller/SearchViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import Kingfisher
 
 struct Library {
     var thumbnail: String
+    var title: String
+    var publisher: String
+    var date: String
+    var price: Int
+    var sale: Int
 }
 
 class SearchViewController: UIViewController {
@@ -18,8 +24,11 @@ class SearchViewController: UIViewController {
     @IBOutlet var leftCloseBarButtonItem: UIBarButtonItem!
     @IBOutlet var bookSearchBar: UISearchBar!
     @IBOutlet var libraryCollectionView: UICollectionView!
+    @IBOutlet var libraryTableView: UITableView!
     
     var library: [Library] = []
+    var page = 1
+    var isEnd = false // 현재 페이지가 마지막 페이지인지 점검하는 프로퍼티
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,12 +37,17 @@ class SearchViewController: UIViewController {
         
         configureLeftCloseBarButtonItem()
         
-        libraryCollectionView.delegate = self
-        libraryCollectionView.dataSource = self
+        //        libraryCollectionView.delegate = self
+        //        libraryCollectionView.dataSource = self
+        libraryTableView.delegate = self
+        libraryTableView.dataSource = self
+        libraryTableView.rowHeight = 150
+        libraryTableView.prefetchDataSource = self
+        
         bookSearchBar.delegate = self
         bookSearchBar.placeholder = "검색어를 입력해주세요."
         
-        setCollectionViewLayout()
+        //        setCollectionViewLayout()
         
     }
     
@@ -47,29 +61,44 @@ class SearchViewController: UIViewController {
     }
     
     
-    func callRequest(voca: String) {
-        let url = "https://dapi.kakao.com/v3/search/book?query=\(voca)"
+    func callRequest(voca: String, page: Int) {
+        guard let text = voca.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        let url = "https://dapi.kakao.com/v3/search/book?query=\(text)&size=10&page=\(page)"
         let header: HTTPHeaders = ["Authorization": "KakaoAK \(APIKey.kakaoKey)"]
+        
+        print(url)
+        
         AF.request(url,
                    method: .get,
-                   headers: header).validate().responseJSON { response in
+                   headers: header).validate(statusCode: 200...500).responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
                 print(json)
                 
-                self.library.removeAll()
+                guard let statusCode = response.response?.statusCode else { return print("에러발생!")}
                 
-                for item in json["documents"].arrayValue {
-                    let thumbnail = item["thumbnail"].stringValue
-                    let data = Library(thumbnail: thumbnail)
-                    self.library.append(data)
+                if statusCode == 200 {
+                    
+                    self.isEnd = json["meta"]["is_end"].boolValue
+                    print(self.isEnd)
+                    
+                    for item in json["documents"].arrayValue {
+                        let thumbnail = item["thumbnail"].stringValue
+                        let title = item["title"].stringValue
+                        let publisher = item["publisher"].stringValue
+                        let date = item["datetime"].stringValue
+                        let price = item["price"].intValue
+                        let sale = item["sale_price"].intValue
+                        
+                        let data = Library(thumbnail: thumbnail, title: title, publisher: publisher, date: date, price: price, sale: sale)
+                        self.library.append(data)
+                    }
+                    self.libraryTableView.reloadData()
+                    
+                } else {
+                    print("문제가 발생했어요. 잠시 후 다시 시도해주세요!")
                 }
-                
-                print(self.library)
-                
-                self.libraryCollectionView.reloadData()
-                
             case .failure(let error):
                 print(error)
             }
@@ -107,34 +136,78 @@ extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSo
         return cell
     }
     
-    func setCollectionViewLayout() {
-        
-        let layout = UICollectionViewFlowLayout()
-        
-        
-        let spacing: CGFloat = 16
-        let width = UIScreen.main.bounds.width - (spacing * 3)
-        
-        layout.scrollDirection = .vertical
-        
-        layout.itemSize = CGSize(width: width / 2, height: width / 2)
-        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
-        
-        layout.minimumLineSpacing = spacing
-        layout.minimumInteritemSpacing = spacing
-        
-        libraryCollectionView.collectionViewLayout = layout
-        
-    }
-    
+    //    func setCollectionViewLayout() {
+    //
+    //        let layout = UICollectionViewFlowLayout()
+    //
+    //
+    //        let spacing: CGFloat = 16
+    //        let width = UIScreen.main.bounds.width - (spacing * 3)
+    //
+    //        layout.scrollDirection = .vertical
+    //
+    //        layout.itemSize = CGSize(width: width / 2, height: width / 2)
+    //        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+    //
+    //        layout.minimumLineSpacing = spacing
+    //        layout.minimumInteritemSpacing = spacing
+    //
+    //        libraryCollectionView.collectionViewLayout = layout
+    //
 }
+
 
 extension SearchViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-        guard let text = searchBar.text else { return }
-        callRequest(voca: text)
+        page = 1
+        library.removeAll()
+        
+        guard let text = bookSearchBar.text else { return }
+        callRequest(voca: text, page: page)
         
     }
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource, UITableViewDataSourcePrefetching {
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        for indexPath in indexPaths {
+            if library.count - 1 == indexPath.row && !isEnd //&& page < 15
+            {
+                page += 1
+                guard let voca = bookSearchBar.text else { return }
+                callRequest(voca: voca, page: page)
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        print("====취소 \(indexPaths)")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return library.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: LibraryTableViewCell.identifier) as? LibraryTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let image = library[indexPath.row].thumbnail
+        let url = URL(string: image)
+        
+        cell.posterImageView.kf.setImage(with: url)
+        cell.bookTitleLabel.text = library[indexPath.row].title
+        
+        cell.publisherLabel.text = "출판사: " + library[indexPath.row].publisher
+        cell.dateLabel.text = "초판날짜: " + library[indexPath.row].date
+        cell.priceLabel.text = "판매가: \(library[indexPath.row].price)원"
+        cell.saleLabel.text = "세일가: \(library[indexPath.row].sale)원"
+        
+        return cell
+    }
+    
 }
